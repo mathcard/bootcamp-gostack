c53b186848b5b5a7893a34da34b11d43b5158f82
mathcard <mathcardoso.94@gmail.com>
2020-11-02 18:31:20 -0300

    Cofigurando eslint, prettier, editorconfig

--- node/projeto1/src/server.ts
+++ node/projeto1/src/server.ts
@@ -5,7 +5,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-
 app.listen(3333, () => {
-  console.log('Server started on port 3333!')
-})
+  console.log('Server started on port 3333!');
+});
