806cd97d30764e80fc2ca304858d4456d099892e
mathcard <mathcardoso.94@gmail.com>
2020-11-12 23:24:43 -0300

    Iniciando aplicacores react

--- reactJS/projeto-react/src/routes/index.tsx
+++ reactJS/projeto-react/src/routes/index.tsx
@@ -4,10 +4,12 @@ import { Switch, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
+// Como o paramentro possua uma /, é necessario colocar o +
+
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
-    <Route path="/repository" component={Repository} />
+    <Route path="/repositories/:repository+" component={Repository} />
   </Switch>
 );
 
