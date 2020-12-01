931b2f8112cf79f8d7586f8d4845e16d940871e9
mathcard <mathcardoso.94@gmail.com>
2020-11-30 22:57:07 -0300

    Iniciando nível 4

---
+++ node/projeto3-gobarberArquitet/src/modules/appointments/services/CreateAppointmentService.ts
@@ -0,0 +1,43 @@
+/* eslint-disable camelcase */
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppError from '@shared/errors/AppError';
+
+interface RequestDTO {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentServices {
+  public async execute({
+    date,
+    provider_id,
+  }: RequestDTO): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    // Transformanda data informada para data inicial
+    const appointmentDate = startOfHour(date);
+
+    // Validando se já existe agendamento na data passada
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentServices;
---
+++ node/projeto3-gobarberArquitet/src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,57 @@
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', async (request, response) => {
+  const { name, email, password } = request.body;
+  const createUser = new CreateUserService();
+
+  const user = await createUser.execute({ name, email, password });
+
+  // Apena não exiber o password no return
+  const userWithoutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
+
+  return response.json(userWithoutPassword);
+});
+
+// Patch quando alterar apenas um dado
+// Put quando permite alterar todos os dados
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    // Apena não exiber o password no return
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json({ userWithoutPassword });
+  },
+);
+
+export default usersRouter;
