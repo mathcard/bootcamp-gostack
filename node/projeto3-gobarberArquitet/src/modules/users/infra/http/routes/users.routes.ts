d43c2437b86425172e8617634c7912507f91214f
mathcard <mathcardoso.94@gmail.com>
2020-12-02 21:50:44 -0300

    Aula 9 Módulo 4

--- node/projeto3-gobarberArquitet/src/modules/users/infra/http/routes/users.routes.ts
+++ node/projeto3-gobarberArquitet/src/modules/users/infra/http/routes/users.routes.ts
@@ -4,13 +4,16 @@ import uploadConfig from '@config/upload';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UsersRepository from '../../typeorm/repositories/UsersRepository';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
+  const usersRepository = new UsersRepository();
   const { name, email, password } = request.body;
-  const createUser = new CreateUserService();
+
+  const createUser = new CreateUserService(usersRepository);
 
   const user = await createUser.execute({ name, email, password });
 
@@ -34,7 +37,9 @@ usersRouter.patch(
   upload.single('avatar'),
   async (request, response) => {
     console.log(request.file);
-    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const usersRepository = new UsersRepository();
+    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
