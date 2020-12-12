7628bf735b4ea275cc82f63db79dbac1d73b2b22
mathcard <mathcardoso.94@gmail.com>
2020-12-12 15:28:57 -0300

    Iniciando envio de e-mails

--- node/projeto3-gobarberArquitet/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ node/projeto3-gobarberArquitet/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,8 +1,9 @@
 import { injectable, inject } from 'tsyringe';
 //import User from '@modules/users/infra/typeorm/entities/User';
-//import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 interface IRequest {
   email: string;
@@ -16,10 +17,27 @@ class SendForgotPasswordEmailService {
 
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
-    this.mailProvider.sendMail(email, 'Pedido de recupareção de senha recebido')
+    const user = await this.usersRepository.findByEmail(email);
+
+    if(!user) {
+      throw new AppError('User does not exists.')
+    }
+
+    const { token } = await this.userTokensRepository.generate(user.id)
+    
+    await this.mailProvider.sendMail(
+      email, 
+      `Pedido de recupareção de senha recebido: ${token}`,
+    );
+
+    
+
   }
 }
 
